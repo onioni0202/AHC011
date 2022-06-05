8bc24397de7d1a130e67dc31327ffc08d6915d7f
onioni0202 <49858062+onioni0202@users.noreply.github.com>
2022-06-05 11:13:17 +0900

    deleted unionfind

--- src/main.rs
+++ src/main.rs
@@ -2,57 +2,8 @@ use proconio::input;
 use proconio::marker::Chars;
 use rand::prelude::*;
 use std::collections::VecDeque;
-
 const SEED: u128 = 0;
-
-struct UnionFind {
-    par: Vec<usize>,
-    siz: Vec<usize>,
-}
-
-#[allow(dead_code)]
-impl UnionFind {
-    fn new(n: usize) -> Self {
-        UnionFind {
-            par: (0..n).collect(),
-            siz: vec![1; n],
-        }
-    }
-
-    fn find(&mut self, x: usize) -> usize {
-        if self.par[x] == x {
-            return x;
-        }
-        self.par[x] = self.find(self.par[x]);
-        self.par[x]
-    }
-
-    fn union(&mut self, mut parent: usize, mut child: usize) -> bool {
-        parent = self.find(parent);
-        child = self.find(child);
-
-        if parent == child {
-            return false;
-        }
-
-        if self.siz[parent] < self.siz[child] {
-            std::mem::swap(&mut parent, &mut child);
-        }
-
-        self.par[child] = parent;
-        self.siz[parent] += self.siz[child];
-        true
-    }
-
-    fn size(&mut self, x: usize) -> usize {
-        let root = self.find(x);
-        self.siz[root]
-    }
-
-    fn issame(&mut self, x: usize, y: usize) -> bool {
-        self.find(x) == self.find(y)
-    }
-}
+const SEARCH_TIME: [f32; 11] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5];
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Board {
@@ -113,33 +64,14 @@ impl Board {
         if th >= self.board_size || tw >= self.board_size {
             return false;
         }
-        self.swap(self.empty_tile_area, (th, tw));
+        self.swap(self.empty_tile_area.0 * self.board_size + self.empty_tile_area.1 , 
+                  th * self.board_size + tw);
         self.empty_tile_area = (th, tw);
         return true;
     }
 
-    fn move_left(&mut self) -> bool {
-        self.move_empty_tile('L')
-    }
-
-    fn move_up(&mut self) -> bool {
-        self.move_empty_tile('U')
-    }
-
-    fn move_right(&mut self) -> bool {
-        self.move_empty_tile('R')
-    }
-
-    fn move_down(&mut self) -> bool {
-        self.move_empty_tile('D')
-    }
-
-    fn swap(&mut self, (h1, w1): (usize, usize), (h2, w2): (usize, usize)) {
-        self.board_list.swap(h1 * self.board_size + w1, h2 * self.board_size + w2);
-    }
-
-    fn replace(&mut self, h: usize, w: usize, value: u8) {
-        self.board_list[h * self.board_size + w] = value;
+    fn swap(&mut self, idx1: usize, idx2: usize) {
+        self.board_list.swap(idx1, idx2);
     }
 
     fn get(&self, h: usize, w: usize) -> u8 {
@@ -190,12 +122,13 @@ fn main() {
         if limit_time <= 0.0 {
             break;
         }
-        let best_board = annealing_search_best_board(&now_board, max_iter, f32::min(0.8, limit_time), &mut rng);
+        let best_board = annealing_search_best_board(&now_board, max_iter, 
+                                        f32::min(SEARCH_TIME[board.board_size], limit_time), &mut rng);
         let greedy_solution = greedy(&mut now_board, &best_board);
         if greedy_solution.len() > max_iter {
             continue;
         }
-        let (now_score, now_solution) = annealing(&board, max_iter, greedy_solution, 0.1, &mut rng);
+        let (now_score, now_solution) = annealing(&board, max_iter, greedy_solution, 0.15, &mut rng);
         if now_score > best_score {
             best_score = now_score;
             best_solution = now_solution;
@@ -208,51 +141,51 @@ fn main() {
 fn greedy(board: &mut Board, best_board: &Board) -> Vec<char> {
     let board_size = board.board_size;
     let mut solution = vec![];
-    let mut fixed = vec![vec![false; board_size]; board_size];
+    let mut fixed = vec![false; board_size * board_size];
 
     'mainloop: for h in 0..board_size - 1 {
         for w in 0..board_size - 1 {
             if h < board_size - 2 && w < board_size - 2 {
                 move_tile(board, best_board, (h, w), (h, w), &mut solution, &mut fixed);
-                fixed[h][w] = true;
+                fixed[h * board_size + w] = true;
             } else if h < board_size - 2 && w == board_size - 2 {
                 if !move_tile(board, best_board, (h, w + 1), (h, w), &mut solution, &mut fixed) {
                     break 'mainloop;
                 }
-                fixed[h][w] = true;
+                fixed[h * board_size + w] = true;
                 if !move_tile(board, best_board, (h, w), (h + 1, w), &mut solution, &mut fixed) {
                     break 'mainloop;
                 }
-                fixed[h + 1][w] = true;
+                fixed[(h + 1) * board_size + w] = true;
                 if !move_empty_tile_to_goal(board, (h, w + 1), &mut solution, &fixed) {
                     break 'mainloop;
                 }
-                board.move_left();
-                board.move_down();
+                board.move_empty_tile('L');
+                board.move_empty_tile('D');
                 solution.push('L');
                 solution.push('D');
-                fixed[h][w] = true;
-                fixed[h][w + 1] = true;
-                fixed[h + 1][w] = false;
+                fixed[h * board_size + w] = true;
+                fixed[h * board_size + (w + 1)] = true;
+                fixed[(h + 1) * board_size + w] = false;
             } else {
                 if !move_tile(board,best_board, (h + 1, w), (h, w), &mut solution, &mut fixed) {
                     break 'mainloop;
                 }
-                fixed[h][w] = true;
+                fixed[h * board_size + w] = true;
                 if !move_tile(board,best_board, (h, w), (h, w + 1), &mut solution, &mut fixed) {
                     break 'mainloop;
                 }
-                fixed[h][w + 1] = true;
+                fixed[h * board_size + w + 1] = true;
                 if !move_empty_tile_to_goal(board, (h + 1, w), &mut solution, &fixed) {
                     break 'mainloop;
                 }
-                board.move_up();
-                board.move_right();
+                board.move_empty_tile('U');
+                board.move_empty_tile('R');
                 solution.push('U');
                 solution.push('R');
-                fixed[h][w] = true;
-                fixed[h + 1][w] = true;
-                fixed[h][w + 1] = false;
+                fixed[h * board_size + w] = true;
+                fixed[(h + 1) * board_size + w] = true;
+                fixed[h * board_size + w + 1] = false;
             }
         }
     }
@@ -265,35 +198,37 @@ fn move_tile(
     best_tile_pos: (usize, usize),
     goal: (usize, usize),
     solution: &mut Vec<char>,
-    fixed: &mut Vec<Vec<bool>>,
+    fixed: &mut Vec<bool>,
 ) -> bool {
+    let board_size = board.board_size;
     let best_tile = best_board.get(best_tile_pos.0, best_tile_pos.1);
     if let Some(tile_positions) = search_tiles(&board, best_tile, goal, &fixed) {
         'mainloop: for mut tile_pos in tile_positions {
-            let tile_pass = find_path(board, tile_pos, goal, fixed);
-            let mut board_copy = board.clone();
-            let mut movement = vec![];
-            for dchar in tile_pass {
-                let didx = Board::from_dchar_to_didx(dchar);
-                fixed[tile_pos.0][tile_pos.1] = true;
-                let nxt_tile_pos = (
-                    tile_pos.0.wrapping_add(Board::DH[didx]),
-                    tile_pos.1.wrapping_add(Board::DW[didx]),
-                );
-                if !move_empty_tile_to_goal(&mut board_copy, nxt_tile_pos, &mut movement, fixed) {
-                    fixed[tile_pos.0][tile_pos.1] = false;
-                    continue 'mainloop;
+            if let Some(tile_path) = find_path(board, tile_pos, goal, fixed) {
+                let mut board_copy = board.clone();
+                let mut movement = vec![];
+                for dchar in tile_path {
+                    let didx = Board::from_dchar_to_didx(dchar);
+                    fixed[tile_pos.0 * board_size + tile_pos.1] = true;
+                    let nxt_tile_pos = (
+                        tile_pos.0.wrapping_add(Board::DH[didx]),
+                        tile_pos.1.wrapping_add(Board::DW[didx]),
+                    );
+                    if !move_empty_tile_to_goal(&mut board_copy, nxt_tile_pos, &mut movement, fixed) {
+                        fixed[tile_pos.0 * board_size + tile_pos.1] = false;
+                        continue 'mainloop;
+                    }
+                    movement.push(Board::DCHARS[(didx + 2) % 4]);
+                    board_copy.move_empty_tile(Board::DCHARS[(didx + 2) % 4]);
+                    fixed[tile_pos.0 * board_size + tile_pos.1] = false;
+                    tile_pos = nxt_tile_pos;
                 }
-                movement.push(Board::DCHARS[(didx + 2) % 4]);
-                board_copy.move_empty_tile(Board::DCHARS[(didx + 2) % 4]);
-                fixed[tile_pos.0][tile_pos.1] = false;
-                tile_pos = nxt_tile_pos;
+                for dchar in movement {
+                    solution.push(dchar);
+                    board.move_empty_tile(dchar);
+                }
+                return true;
             }
-            for dchar in movement {
-                solution.push(dchar);
-                board.move_empty_tile(dchar);
-            }
-            return true;
         }
     }
     return false;
@@ -303,27 +238,28 @@ fn move_empty_tile_to_goal(
     board: &mut Board,
     goal: (usize, usize),
     solution: &mut Vec<char>,
-    fixed: &Vec<Vec<bool>>,
+    fixed: &Vec<bool>,
 ) -> bool {
     if goal == board.empty_tile_area {
         return true;
     }
-    let empty_tile_pass = find_path(board, board.empty_tile_area, goal, fixed);
-    if empty_tile_pass.is_empty() {
-        return false;
+    if let Some(empty_tile_path) = find_path(board, board.empty_tile_area, goal, fixed) {
+        for dchar in empty_tile_path {
+            solution.push(dchar);
+            board.move_empty_tile(dchar);
+        }
+        return true;
     }
-    for dchar in empty_tile_pass {
-        solution.push(dchar);
-        board.move_empty_tile(dchar);
-    }
-    return true;
+    return false;
+    
 }
 
-fn search_tiles(board: &Board, tile: u8, start: (usize, usize), fixed: &Vec<Vec<bool>>) -> Option<Vec<(usize, usize)>> {
+fn search_tiles(board: &Board, tile: u8, start: (usize, usize), fixed: &Vec<bool>) -> Option<Vec<(usize, usize)>> {
+    let board_size = board.board_size;
     let mut que = VecDeque::new();
-    let mut seen = vec![vec![false; board.board_size]; board.board_size];
+    let mut seen = vec![false; board_size * board_size];
     que.push_back(start);
-    seen[start.0][start.1] = true;
+    seen[start.0 * board_size + start.1] = true;
     let mut positions = vec![];
     while let Some((h_now, w_now)) = que.pop_front() {
         if board.get(h_now, w_now) == tile {
@@ -334,11 +270,11 @@ fn search_tiles(board: &Board, tile: u8, start: (usize, usize), fixed: &Vec<Vec<
                 h_now.wrapping_add(Board::DH[didx]),
                 w_now.wrapping_add(Board::DW[didx]),
             );
-            if h_to >= board.board_size || w_to >= board.board_size {
+            if h_to >= board_size || w_to >= board_size {
                 continue;
             }
-            if !fixed[h_to][w_to] && !seen[h_to][w_to] {
-                seen[h_to][w_to] = true;
+            if !fixed[h_to * board_size + w_to] && !seen[h_to * board_size + w_to] {
+                seen[h_to * board_size + w_to] = true;
                 que.push_back((h_to, w_to));
             }
         }
@@ -353,13 +289,14 @@ fn find_path(
     board: &Board,
     start: (usize, usize),
     goal: (usize, usize),
-    fixed: &Vec<Vec<bool>>,
-) -> Vec<char> {
+    fixed: &Vec<bool>,
+) -> Option<Vec<char>> {
+    let board_size = board.board_size;
     let mut que = VecDeque::new();
-    let mut dist = vec![vec![std::i32::MAX; board.board_size]; board.board_size];
-    let mut prev = vec![vec![None; board.board_size]; board.board_size];
+    let mut dist = vec![std::i32::MAX; board_size * board_size];
+    let mut prev = vec![None; board_size * board_size];
     que.push_back(start);
-    dist[start.0][start.1] = 0;
+    dist[start.0 * board_size + start.1] = 0;
     while let Some((h_now, w_now)) = que.pop_front() {
         if (h_now, w_now) == goal {
             break;
@@ -369,30 +306,30 @@ fn find_path(
                 h_now.wrapping_add(Board::DH[didx]),
                 w_now.wrapping_add(Board::DW[didx]),
             );
-            if h_to >= board.board_size || w_to >= board.board_size {
+            if h_to >= board_size || w_to >= board_size {
                 continue;
             }
-            if fixed[h_to][w_to] {
+            if fixed[h_to * board_size + w_to] {
                 continue;
             }
-            if dist[h_to][w_to] > dist[h_now][w_now] + 1 {
-                dist[h_to][w_to] = dist[h_now][w_now] + 1;
-                prev[h_to][w_to] = Some(((h_now, w_now), Board::DCHARS[didx]));
+            if dist[h_to * board_size + w_to] > dist[h_now * board_size + w_now] + 1 {
+                dist[h_to * board_size + w_to] = dist[h_now * board_size + w_now] + 1;
+                prev[h_to * board_size + w_to] = Some(((h_now, w_now), Board::DCHARS[didx]));
                 que.push_back((h_to, w_to));
             }
         }
     }
-    if prev[goal.0][goal.1].is_none() {
-        return vec![];
+    if prev[goal.0 * board_size + goal.1].is_none() {
+        return None;
     }
-    let mut pass = vec![];
+    let mut path = vec![];
     let mut now_pos = goal;
-    while let Some((nxt_pos, didx)) = prev[now_pos.0][now_pos.1] {
+    while let Some((nxt_pos, dchar)) = prev[now_pos.0 * board_size + now_pos.1] {
         now_pos = nxt_pos;
-        pass.push(didx);
+        path.push(dchar);
     }
-    pass.reverse();
-    pass
+    path.reverse();
+    Some(path)
 }
 
 fn annealing_search_best_board(
@@ -406,38 +343,41 @@ fn annealing_search_best_board(
     let start_time = std::time::Instant::now();
     let board_size = init_board.board_size;
     let mut board = init_board.clone();
-    let empty_tile_position = (board_size - 1, board_size - 1);
-    board.swap(board.empty_tile_area, empty_tile_position);
+    let empty_tile_position = board_size * board_size - 1;
+    board.swap(board.empty_tile_area.0 * board_size + board.empty_tile_area.1, empty_tile_position);
     board.update_empty_tile_area();
     let mut score = calc_score(&board, 0, max_iter);
     let mut best_board = board.clone();
     let mut best_score = score;
+    let mut iter_num = 0;
     loop {
         let diff_time = (std::time::Instant::now() - start_time).as_secs_f32();
+        iter_num += 1;
         if diff_time > duration {
             break;
         }
-        let mut new_board = board.clone();
-        let choice1 = (rng.gen_range(0, board_size), rng.gen_range(0, board_size));
-        let choice2 = (rng.gen_range(0, board_size), rng.gen_range(0, board_size));
-        if choice1 == choice2 || choice1 == empty_tile_position || choice2 == empty_tile_position {
+        let choice1 = rng.gen_range(0, board_size * board_size - 2);
+        let choice2 = rng.gen_range(choice1 + 1, board_size * board_size - 1);
+        if choice1 == choice2 {
             continue;
         }
-        new_board.swap(choice1, choice2);
-        let new_score = calc_score(&new_board, 0, max_iter);
+        board.swap(choice1, choice2);
+        let new_score = calc_score(&board, 0, max_iter);
         let temp = START_TEMP + (END_TEMP - START_TEMP) * diff_time / duration;
-        if f32::exp((new_score - score) as f32 / temp) > rng.gen() {
-            score = new_score;
-            board = new_board.clone();
-        }
         if new_score > best_score {
             best_score = new_score;
-            best_board = new_board;
+            best_board = board.clone();
             if best_score > 5e5 {
                 break;
             }
         }
+        if f32::exp((new_score - score) as f32 / temp) > rng.gen() {
+            score = new_score;
+        } else {
+            board.swap(choice1, choice2);
+        }
     }
+    eprintln!("search iter num = {}, best score = {}", iter_num, best_score);
     best_board.update_empty_tile_area();
     best_board
 }
@@ -476,8 +416,8 @@ fn annealing(
                 if new_solution.len() < max_iter / 2 {
                     continue;
                 }
-                let select1 = rng.gen_range(0, new_solution.len());
-                let select2 = rng.gen_range(0, new_solution.len());
+                let select1 = rng.gen_range(0, new_solution.len() - 1);
+                let select2 = rng.gen_range(select1 + 1, new_solution.len());
                 new_solution.swap(select1, select2);
             }
             1 => {
@@ -524,6 +464,9 @@ fn annealing(
             }
         }
         let new_score = calc_score(&new_board, new_solution.len(), max_iter);
+        if iter_num == 1 && new_score < 4.5e5 {
+            return (new_score, new_solution);
+        }
         let temp = START_TEMP + (END_TEMP - START_TEMP) * diff_time / duration;
         if f32::exp((new_score - score) / temp) > rng.gen() {
             score = new_score;
@@ -541,15 +484,15 @@ fn annealing(
 
 fn calc_score(board: &Board, iter: usize, max_iter: usize) -> f32 {
     let board_size = board.board_size;
-    let mut passed = vec![vec![false; board_size]; board_size];
+    let mut passed = vec![false; board_size * board_size];
     let mut que = VecDeque::new();
-    let mut uf = UnionFind::new(board_size * board_size);
     let mut max_tree_size = 0;
+    let mut score = 0.0;
     for h_st in 0..board_size {
         for w_st in 0..board_size {
-            if !passed[h_st][w_st] {
+            if !passed[h_st * board_size + w_st] {
                 let mut tree_size = 0;
-                passed[h_st][w_st] = true;
+                passed[h_st * board_size + w_st] = true;
                 que.push_back(((h_st, w_st), 10));
                 while let Some(((h_now, w_now), prev)) = que.pop_front() {
                     tree_size += 1;
@@ -566,12 +509,8 @@ fn calc_score(board: &Board, iter: usize, max_iter: usize) -> f32 {
                                 continue;
                             }
                             if ((board.get(h_to, w_to) >> ((didx + 2) % 4)) & 1) == 1 {
-                                if uf.issame(h_now * board_size + w_now, h_to * board_size + w_to) {
-                                    tree_size -= 10000;
-                                }
-                                uf.union(h_now * board_size + w_now, h_to * board_size + w_to);
-                                if !passed[h_to][w_to] {
-                                    passed[h_to][w_to] = true;
+                                if !passed[h_to * board_size + w_to] {
+                                    passed[h_to * board_size + w_to] = true;
                                     que.push_back(((h_to, w_to), didx));
                                 }
                             }
@@ -583,8 +522,9 @@ fn calc_score(board: &Board, iter: usize, max_iter: usize) -> f32 {
         }
     }
     if max_tree_size == (board_size * board_size - 1) as i32 {
-        5e5 * (2.0 - (iter as f32 / max_iter as f32))
+        score += 5e5 * (2.0 - (iter as f32 / max_iter as f32));
     } else {
-        5e5 * (max_tree_size as f32) / (board_size * board_size - 1) as f32
+        score += 5e5 * (max_tree_size as f32) / (board_size * board_size - 1) as f32;
     }
+    score
 }
